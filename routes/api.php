6ea1e0329e8c0a3951eeb87d81d655fd45a4f58f
Jeriken <mrazin777@gmail.com>
2024-06-19 10:03:12 +0700

    feat: Create post API

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\PostController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,4 +12,12 @@ Route::get('/user', function (Request $request) {
 Route::prefix('auth')->group(function () {
     Route::post('/register', [AuthController::class, 'register'])->name('register');
     Route::post('/login', [AuthController::class, 'login'])->name('login');    
+});
+
+Route::prefix('posts')->middleware('auth:api')->group(function () {
+    Route::get('/', [PostController::class, 'index'])->name('index');    
+    Route::get('/{id}', [PostController::class, 'show'])->name('show');    
+    Route::post('/', [PostController::class, 'store'])->name('store');    
+    Route::put('/{id}', [PostController::class, 'update'])->name('update');   
+    Route::delete('/{id}', [PostController::class, 'destroy'])->name('destroy');    
 });
