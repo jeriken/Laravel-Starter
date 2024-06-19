d5dc6e3681804a4ba07ce3145118420e580fd963
Jeriken <mrazin777@gmail.com>
2024-06-19 19:48:18 +0700

    feat: Create get profile api

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\ProfileController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -20,4 +21,8 @@ Route::prefix('posts')->middleware('auth:api')->group(function () {
     Route::post('/', [PostController::class, 'store'])->name('store');    
     Route::put('/{id}', [PostController::class, 'update'])->name('update');   
     Route::delete('/{id}', [PostController::class, 'destroy'])->name('destroy');    
+});
+
+Route::prefix('profile')->middleware('auth:api')->group(function () {
+    Route::get('/', [ProfileController::class, 'index'])->name('index');  
 });
